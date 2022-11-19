b3c6fae6c48e1be884ed442499f605c9b7f67462
dapav <82372150+dapav@users.noreply.github.com>
2022-11-19 14:58:32 +0200

    Basic testing from tutorial

---
+++ src/test/java/com/university/uniweb/professor/ProfessorRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.university.uniweb.professor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+//@DataJpaTest
+class ProfessorRepositoryTest {
+
+    @Autowired
+    private  ProfessorRepository professorRepository;
+
+    @Test
+    public void saveProfessor(){
+        Professor professor = Professor.builder()
+                .firstName("Nikolas")
+                .lastName("Samaras")
+                .email("samNik@uom.edu.gr")
+                .build();
+
+        professorRepository.save(professor);
+    }
+
+    @Test
+    public void printAllProfessors(){
+        List<Professor> professorList = professorRepository.findAll();
+
+        System.out.println("Professors List: " + professorList);
+    }
+}
